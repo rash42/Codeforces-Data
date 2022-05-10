341dc3694f5ba88b95863964da8bdde1953aac58
unknown <Dark-Supreme>
2022-05-11 01:08:04 +0530

    Added table in contest page

--- src/components/ContestInfo.js
+++ src/components/ContestInfo.js
@@ -5,7 +5,7 @@ function ContestInfo(props) {
     var dt = new Date(props.info.startTimeSeconds*1000)
     console.log(dt.toLocaleTimeString())
     return (
-        <div className="card user">
+/*        <div className="card user">
             <h5 className="card-header">{props.info.name}</h5>
             <div className="card-body">
                 <h5 className="card-title">Contest Id: {props.info.id}</h5>
@@ -13,8 +13,15 @@ function ContestInfo(props) {
                 <p className="card-text">Duration: {props.info.durationSeconds}</p>
                 <p className="card-text">Start Time: {dt.toDateString()} {dt.toLocaleTimeString()}</p>
             </div>
+        </div>*/
 
-        </div>
+        <tr>
+            <td>{props.info.id}</td>
+            <td>{props.info.type}</td>
+            <td>{props.info.name}</td>
+            <td>{props.info.durationSeconds/60} Mins</td>
+            <td>{dt.toDateString()} {dt.toLocaleDateString()}</td>
+        </tr>
 
 
     )
